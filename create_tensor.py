f7a3b979ad8d9c2a3621af71bc03d80a4282e571
wmg <ebeviswong@126.com>
2020-01-20 17:14:32 +0800

    creater_tensor 07 page 07.

--- create_tensor.py
+++ create_tensor.py
@@ -10,3 +10,28 @@ d = torch.from_numpy(c)
 
 e = np.zeros([4, 4])
 print("d = {}".format(d))
+
+f = torch.FloatTensor([3.1, 3.2])
+print("f = {}".format(f))
+
+# uninitialized.
+g = torch.empty(2)
+print("g = {}".format(g))
+
+h = torch.Tensor(3, 3)
+i = torch.IntTensor(3, 3)
+j = torch.FloatTensor(2, 2)
+
+print("the type of tensor is {}".format(h.type()))
+
+# If you input the shape of tensor, the created tensor is uninitialized.
+# If the val is input, the created is initialized by val.
+
+# set default type.
+torch.set_default_tensor_type(torch.DoubleTensor)
+
+
+a_tensor = torch.tensor([1.0, 2.0, 3.0])
+print("the type of a_tensor is {}".format(a_tensor.type()))
+
+pass
